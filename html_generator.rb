ed94e58f2cd680b80bf10e8a5c90de35a331b74c
Khurram Virani <kvirani@gmail.com>
2013-03-18 11:39:04 -0400

    Print header and footer methods (private)

--- html_generator.rb
+++ html_generator.rb
@@ -1,11 +1,30 @@
 class HtmlGenerator
   
-  def self.show
+  def show
+    print_header
     puts "Action: Show"
+    print_footer
   end
 
-  def self.index
+  def index
+    print_header
     puts "Action: Index"
+    print_footer
+  end
+
+  private
+
+  def print_header
+    puts "<html>"
+    puts "  <head>"
+    puts "    <title>Connoisseur</title>"
+    puts "  </head>"
+    puts "  <body>"
+  end
+
+  def print_footer
+    puts "  </body>"
+    puts "</html>"
   end
 
 end
--- router.rb
+++ router.rb
@@ -4,10 +4,9 @@ require_relative 'html_generator'
 if ARGV.empty? # there are no arguments provided
   puts "USAGE: bla bla bla"
 else
-  puts "Ya, you passed in some arguments :)"
   generator = HtmlGenerator.new
   if ARGV[0] == "index"
-    generator.index  
+    generator.index
   elsif ARGV[0] == "show"
     generator.show
   else
