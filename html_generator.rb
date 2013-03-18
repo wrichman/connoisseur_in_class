53329928a1832e9c1a942cf52a12372c63ba02a2
Khurram Virani <kvirani@gmail.com>
2013-03-18 10:53:16 -0400

    Created HtmlGenerator to handle actual work

---
+++ html_generator.rb
@@ -0,0 +1,11 @@
+class HtmlGenerator
+  
+  def self.show
+    puts "Action: Show"
+  end
+
+  def self.index
+    puts "Action: Index"
+  end
+
+end
--- router.rb
+++ router.rb
@@ -1,14 +1,15 @@
-# Access parameters passed into the app
+require_relative 'html_generator'
+
 
 if ARGV.empty? # there are no arguments provided
   puts "USAGE: bla bla bla"
 else
   puts "Ya, you passed in some arguments :)"
-  
+  generator = HtmlGenerator.new
   if ARGV[0] == "index"
-    puts "Action: index"
+    generator.index  
   elsif ARGV[0] == "show"
-    puts "Action: show"
+    generator.show
   else
     puts "Unrecognized action. Please refer to USAGE."
   end
